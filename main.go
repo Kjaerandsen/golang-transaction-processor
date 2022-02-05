0834b5f7a6aced4601c1bfd8ea1a968922b58352
jonakj <jonakj@stud.ntnu.no>
2022-02-05 11:39:45 +0100

    Basic structure created

---
+++ main.go
@@ -0,0 +1,37 @@
+package golang_two
+
+// Takes flags to run the different functions.
+func main() {
+
+}
+
+// Takes n and generates n rows in a text file, each containing a random number between 0.01 and 99.99
+// with a uniform random distribution, this is written to the file txs.txt
+func generateRandomTxs(n int) {
+
+}
+
+// Reads the txs.txt file, sums all the numbers and prints the result.
+func sum() {
+
+}
+
+// Reads the txs.txt file, generates fees for each row and puts them into the fees.txt file with the same structure.
+func generateFees() {
+
+}
+
+// Calculates the earnings of the app provider (70%) and puts it into the earnings.txt file.
+func earnings() {
+
+}
+
+// Calculates two numbers, fees sum - fees total and total - total earnings + fees sum
+func compare() {
+
+}
+
+// Same as generateRandomTxs, but for a million values.
+func generateMillionTxs() {
+
+}
