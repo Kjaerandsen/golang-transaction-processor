b75a95fe7025b94c3fe944247bb64a44655615b8
jonakj <jonakj@stud.ntnu.no>
2022-02-07 23:18:55 +0100

    Sum function

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math"
 	"math/rand"
 	"os"
+	"strconv"
 )
 
 // Takes flags to run the different functions.
 func main() {
-	generateRandomTxs(1000)
+	//generateRandomTxs(1000)
+	sum()
 }
 
 // Takes n and generates n rows in a text file, each containing a random number between 0.01 and 99.99
@@ -48,7 +51,37 @@ func generateRandomTxs(n int) {
 
 // Reads the txs.txt file, sums all the numbers and prints the result.
 func sum() {
+	var sum float64
+	var add float64
 
+	// Open the file
+	inputFile, err := os.Open("txs.txt")
+	if err != nil {
+		fmt.Println("Error accessing txs.txt file.")
+		return
+	}
+
+	// Reads the file line by line, using code from https://golangdocs.com/golang-read-file-line-by-line
+	scanner := bufio.NewScanner(inputFile)
+
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		// Add the line to the sum
+		fmt.Println(scanner.Text())
+		// Add the line to the add value, if invalid input return an error
+		add, err = strconv.ParseFloat(scanner.Text(), 32)
+		if err != nil {
+			fmt.Println("Error: txs.txt contains invalid values. Please refer to documentation to generate" +
+				" a valid file.")
+			return
+		}
+		// Add the add value to the sum
+		sum = sum + add
+	}
+
+	// Print the result
+	fmt.Println((math.Round(sum * 100)) / 100)
 }
 
 // Reads the txs.txt file, generates fees for each row and puts them into the fees.txt file with the same structure.
